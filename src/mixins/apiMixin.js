decedff476af72f68516866f340edb60b3f5f506
Ruby Chen <mp4bj6ruby@gmail.com>
2016-09-24 23:47:14 +0800

    added apiMixin

---
+++ src/mixins/apiMixin.js
@@ -0,0 +1,42 @@
+import apiConfig from '../../config/api'
+export default {
+  computed: {
+    api() {
+      var router = {
+        setting: {
+          getList: {path: "/setting/get-list", type: "get"},
+          getItem: {path: "/setting/edit", type: "post"},
+          postNew: {path: "/setting/do-add", type: "post"},
+          delItem: {path: "/setting/do-del", type: "post"},
+          updateItem: {path: "/setting/do-edit", type: "post"}
+        }
+      }
+
+      var apiInit = function (method,url,data) {
+        return $.ajax({
+          method: method,
+          url: apiConfig.host + url,
+          data: data,
+          xhrFields: {
+            withCredentials: true
+          }
+        })
+      }
+
+      return {
+        setting: function(subject,action,data) {
+          var route = apiConfig.testMode? 'static/data/getTestList.json' : subject + router.setting[action].path
+          var type = apiConfig.testMode? 'get' : router.setting[action].type
+          return apiInit( type , route , data)
+        },
+        login: function(data) {
+          return apiInit('post','account/common/do-login', data)
+        },
+        menu: function() {
+          var route = apiConfig.testMode? 'static/data/getTestMenu.json' : 'node/menu/get-menu'
+          return apiInit('get', route)
+        }
+      }
+    }
+  }
+}
